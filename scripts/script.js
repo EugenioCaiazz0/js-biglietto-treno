fd7dd0c3720b5df7996dc7b8ec0c0257d442e363
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-02-21 10:00:43 +0100

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,29 @@
 
-const etaPasseggero = prompt("Scrivere la propria età");
+const etaPasseggero = parseFloat(prompt("Scrivere la propria età"));
 
-const kilometriViaggio = prompt("Inserire lunghezza tragitto");
+console.log(etaPasseggero);
 
-let costoViaggio = kilometriViaggio * 0.21 * scontoViaggio;
+const kilometriViaggio = parseFloat(prompt("Inserire lunghezza tragitto"));
+
+console.log(kilometriViaggio);
+
+let scontoViaggio = 0.0;
 
 if (etaPasseggero < 19) {
     scontoViaggio = 0.80
 } 
 else if (etaPasseggero > 65) {
     scontoViaggio = 0.60
-} 
+} else {
+    scontoViaggio = 1.0;
+}
 
-let costoViaggioFixed = costoViaggio.toFixed(2);
+const costoViaggio = kilometriViaggio * 0.21 * scontoViaggio;
+
+console.log(costoViaggio);
+
+const costoViaggioFixed = costoViaggio.toFixed(2);
 
 document.getElementById('stampa-costo').innerHTML = 
 ` <p>Il prezzo del tuo biglietto è di ${costoViaggioFixed}€.</p> `;
+ 
